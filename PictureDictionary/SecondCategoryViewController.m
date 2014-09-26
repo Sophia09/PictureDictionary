6b917970626550f34b0a70b75609e3a994475d35
Sophia09 <lisai121121@gmail.com>
2014-09-26 11:00:46 +0800

    add dealloc method for debug

--- PictureDictionary/SecondCategoryViewController.m
+++ PictureDictionary/SecondCategoryViewController.m
@@ -69,6 +69,11 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)dealloc
+{
+    NSLog(@"SecondCategoryViewController dealloc");
+}
+
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
